738fbe2eb48f21d16367e1f6477c900471edae5a
acmiit <2476854895@qq.com>
2025-09-26 14:05:06 +0800

    update: 2025-09-26 14:05:06

---
+++ Linux/02_linux_system_program/file_and_file_system/IO_multiplexing/0_azhen_select.c
@@ -0,0 +1,31 @@
+#include<54func.h>
+int main(int argc,char* argv[]){
+    // ./0_azhen_select 1.pipe 2.pipe
+    ARGS_CHECK(argc,3);
+    int fdw = open(argv[1],O_WRONLY);
+    ERROR_CHECK(fdw,-1,"open write");
+    int fdr = open(argv[2],O_RDONLY);
+    ERROR_CHECK(fdr,-1,"open read");
+    printf("azhen is connected!\n");
+    char buf[4096];
+    fd_set rdset;
+    while(1){
+        //select之前设置监听
+        FD_ZERO(&rdset);
+        FD_SET(STDIN_FILENO,&rdset);
+        FD_SET(fdr,&rdset);
+        select(fdr+1,&rdset,NULL,NULL,NULL);
+        //select返回之后，说明fdr or stdin就绪了 rdset现在是就绪集合
+        if(FD_ISSET(fdr,&rdset)){
+            memset(buf,0,sizeof(buf));
+            read(fdr,buf,sizeof(buf));
+            printf("buf=%s\n",buf);
+        }
+        if(FD_ISSET(STDIN_FILENO,&rdset)){
+            memset(buf,0,sizeof(buf));
+            read(STDERR_FILENO,buf,sizeof(buf));
+            write(fdw,buf,strlen(buf));
+        }
+    }
+    return 0;
+}
