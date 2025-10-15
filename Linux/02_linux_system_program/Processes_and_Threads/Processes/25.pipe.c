d02272136a46bdab11f623e1f06e9175829e0395
acmiit <2476854895@qq.com>
2025-10-15 21:52:50 +0800

    update: 2025-10-15 21:52:50

--- Linux/02_linux_system_program/Processes_and_Threads/Processes/25.pipe.c
+++ Linux/02_linux_system_program/Processes_and_Threads/Processes/25.pipe.c
@@ -4,10 +4,10 @@ int main(int argc,char argv[]){
     pipe(fds);
     printf("fds[0]=%d, fds[1]=%d\n",fds[0],fds[1]);
     char buf[1024];
+    //先读后写会阻塞在read
     write(fds[1],"Hello",5);
     read(fds[0],buf,sizeof(buf));
     printf("buf = %s\n",buf);
-
     close(fds[0]);
     close(fds[1]);
     return 0;
